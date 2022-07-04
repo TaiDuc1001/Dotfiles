fb0fed509265a70bdc2bf770653e630c5336c580
DucPhan <phantaiduc2005@gmail.com>
2022-07-04 14:32:36 +0700

    Modified in order to test

--- .config/powershell/Ps_profile.ps1
+++ .config/powershell/Ps_profile.ps1
@@ -1,9 +1,13 @@
 # Prompt
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt neko
+Set-PoshPrompt atomic
 
 # Alias
 Set-alias vim nvim
 Set-alias g git 
 Set-alias l recurse 
+
+#Clear
+clear
+
--- .config/powershell/windowPS_profile.ps1
+++ .config/powershell/windowPS_profile.ps1
@@ -6,3 +6,8 @@ Set-PoshPrompt atomic
 # Alias
 Set-alias vim nvim
 Set-alias g git 
+
+#Clear
+clear
+
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Console" -Name "FaceName" -Value "Hack NF" -Type String;
